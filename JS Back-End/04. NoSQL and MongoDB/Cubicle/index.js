7bd0a75db0fa0a6c163802d7690f514a68f9f760
kirencevr <74102364+kirencevr@users.noreply.github.com>
2021-06-08 23:48:20 +0300

    added config files

--- JS Back-End/04. NoSQL and MongoDB/Cubicle/index.js
+++ JS Back-End/04. NoSQL and MongoDB/Cubicle/index.js
@@ -1,13 +1,9 @@
 // initialize Express and HBS
 const express = require('express');
-const hbs = require('express-handlebars');
-const { catalog } = require('./controllers/catalog.js');
-const { defaultHandler } = require('./controllers/default.js');
-const { about } = require('./controllers/about.js');
-const { create, post } = require('./controllers/create.js');
-const { details } = require('./controllers/details.js');
-const { init } = require('./models/storage.js');
-const { edit, update } = require('./controllers/edit.js');
+const expressConfig = require('./config/express.js');
+const databaseConfig = require('./config/database.js');
+const routesConfig = require('./config/routes.js');
+const { init: storage } = require('./services/storage.js');
 const port = 3030;
 
 start();
@@ -15,30 +11,14 @@ start();
 async function start() {
     const app = express();
 
-    app.engine('.hbs', hbs({
-        extname: '.hbs'
-    }));
-
-    app.set('view engine', '.hbs');
-    // setup Static Files
-    app.use('/static', express.static('static'));
-    // setup form-parser
-    app.use(express.urlencoded({ extended: true }));
+    // configure express
+    expressConfig(app);
+    // setup DB
+    await databaseConfig(app);
     // setup storage middleware
-    app.use(await init());
-
-    // setup route handlers
-    app.get('/', catalog);
-    app.get('/about', about);
-    app.get('/details/:id', details);
-    app.get('/create', create);
-    app.post('/create', post);
-    app.get('/edit/:id', edit);
-    app.post('/edit/:id', update);
-
-    // setup default handler
-    app.all('*', defaultHandler);
-
+    app.use(await storage());
+    // setup routes
+    routesConfig(app);
     // start app
     app.listen(port, () => console.log('Server listening on port ' + port));
 }
