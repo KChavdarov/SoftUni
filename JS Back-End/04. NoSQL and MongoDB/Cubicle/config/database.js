7aa3ef3ceb668a66fa9f201bbaa576d6a28ed3a7
kirencevr <74102364+kirencevr@users.noreply.github.com>
2021-06-08 23:48:37 +0300

    added config files

---
+++ JS Back-End/04. NoSQL and MongoDB/Cubicle/config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const connectionString = 'mongodb://localhost:27017/cubicle';
+
+module.exports = (app) => {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(connectionString, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+        const db = mongoose.connection;
+        db.on('open', () => {
+            console.log('Database connected!');
+            resolve('');
+        });
+        db.on('error', (err) => {
+            console.error(err);
+            reject(err.message);
+        });
+    });
+};
---
+++ JS Back-End/04. NoSQL and MongoDB/Cubicle/config/routes.js
@@ -0,0 +1,21 @@
+const { catalog } = require('../controllers/catalog.js');
+const { defaultHandler } = require('../controllers/default.js');
+const { about } = require('../controllers/about.js');
+const { create, post } = require('../controllers/create.js');
+const { details } = require('../controllers/details.js');
+const { edit, update } = require('../controllers/edit.js');
+
+
+module.exports = (app) => {
+    // setup route handlers
+    app.get('/', catalog);
+    app.get('/about', about);
+    app.get('/details/:id', details);
+    app.get('/create', create);
+    app.post('/create', post);
+    app.get('/edit/:id', edit);
+    app.post('/edit/:id', update);
+
+    // setup default handler
+    app.all('*', defaultHandler);
+};
