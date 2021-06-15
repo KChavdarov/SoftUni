10ce53f646c458c2bc889d8ed02b10991fabf32c
kirencevr <74102364+kirencevr@users.noreply.github.com>
2021-06-15 19:17:13 +0300

    converted to cookie parser to middleware

--- JS Back-End/05. Sessions and Authentication/Simple Cookie Parser Demo/index.js
+++ JS Back-End/05. Sessions and Authentication/Simple Cookie Parser Demo/index.js
@@ -4,21 +4,38 @@ const app = express();
 
 const sessions = {};
 
-app.get('/', (req, res) => {
+function mySessionStorage(req, res, next) {
     let session = {};
     if (req.headers.cookie) {
         const id = req.headers.cookie.split('=')[1];
-        session = sessions[id];
-        console.log('Existing user session. ID: ' + id);
+        if (sessions[id] == undefined) {
+            console.log('Invalid user session. Generating new...');
+            createNewSession();
+        } else {
+            session = sessions[id];
+            console.log('Existing user session. ID: ' + id);
+        }
     } else {
+        createNewSession();
+    }
+    session.visited++;
+
+    req.session = session;
+    next();
+
+    function createNewSession() {
         const id = ('0000000' + (Math.random() * 99999999 | 0).toString(16)).slice(-8);
         sessions[id] = session;
         session.visited = 0;
         res.setHeader('Set-Cookie', 'sessionId=' + id);
         console.log('New user session generated. ID: ' + id);
     }
-    session.visited++;
-    res.send('<h1>Hello!</h1><p>You have visited this web page ' + session.visited + ' number of times</p>');
+}
+
+app.use(mySessionStorage);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello!</h1><p>You have visited this web page ' + req.session.visited + ' number of times</p>');
 });
 
 app.listen(3030);
