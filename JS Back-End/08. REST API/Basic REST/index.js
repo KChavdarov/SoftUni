2da2969d6f6fbf8bf5573036fc7bc0cebdc49174
kirencevr <74102364+kirencevr@users.noreply.github.com>
2021-06-29 01:14:50 +0300

    added missing semicolon

--- JS Back-End/08. REST API/Basic REST/index.js
+++ JS Back-End/08. REST API/Basic REST/index.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-})
+});
 app.use(express.json());
 app.use('/api', dataController);
 
