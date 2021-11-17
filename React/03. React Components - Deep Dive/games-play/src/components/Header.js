e12abbf7326eb2370f6fc967b5dcc89e73dbf0cf
KChavdarov <k.chavdarov92@gmail.com>
2021-11-17 22:04:33 +0200

    added routing to site navigation

--- React/03. React Components - Deep Dive/games-play/src/components/Header.js
+++ React/03. React Components - Deep Dive/games-play/src/components/Header.js
@@ -1,25 +1,18 @@
-export function Header({navigator}) {
-    function redirectHandler(event) {
-        if (event && event.target.tagName === 'A') {
-            event.preventDefault();
-            const url = new URL(event.target.href);
-            const [name, id] = url.pathname.split('/').filter(a => a);
-            navigator(name, id);
-        }
-    }
+import {NavLink} from 'react-router-dom';
 
+export function Header() {
     return (
-        <header onClick={redirectHandler}>
-            <h1><a className="home" href="/home">GamesPlay</a></h1>
+        <header>
+            <h1><NavLink className="home" to="/">GamesPlay</NavLink></h1>
             <nav>
-                <a href="/catalogue">All games</a>
+                <NavLink to="/games">All games</NavLink>
                 <div id="user">
-                    <a href="/create">Create Game</a>
-                    <a href="#">Logout</a>
+                    <NavLink to="/create">Create Game</NavLink>
+                    <NavLink to="#">Logout</NavLink>
                 </div>
                 <div id="guest">
-                    <a href="/login">Login</a>
-                    <a href="/register">Register</a>
+                    <NavLink to="/login">Login</NavLink>
+                    <NavLink to="/register">Register</NavLink>
                 </div>
             </nav>
         </header>
