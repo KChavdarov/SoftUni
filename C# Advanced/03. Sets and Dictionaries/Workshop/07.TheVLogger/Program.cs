f026e3e3d281342aca77eddc55bd490acc1a7a0a
KChavdarov <k.chavdarov92@gmail.com>
2022-01-22 21:56:25 +0200

    solved bonus problems

---
+++ C# Advanced/03. Sets and Dictionaries/Workshop/07.TheVLogger/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace _07.TheVLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            SortedDictionary<string, Dictionary<string, SortedSet<string>>> vLogger = new SortedDictionary<string, Dictionary<string, SortedSet<string>>>();
+
+            while (input != "Statistics")
+            {
+                string[] tokens = input.Split(' ');
+                string name = tokens[0];
+                string command = tokens[1];
+
+                switch (command)
+                {
+                    case "joined":
+                        if (!vLogger.ContainsKey(name))
+                        {
+                            vLogger[name] = new Dictionary<string, SortedSet<string>>() {
+                                { "followers", new SortedSet<string>() },
+                                { "following", new SortedSet<string>() }
+                            };
+                        }
+                        break;
+                    case "followed":
+                        string target = tokens[2];
+
+                        if (vLogger.ContainsKey(name) && vLogger.ContainsKey(target) && target != name)
+                        {
+                            vLogger[target]["followers"].Add(name);
+                            vLogger[name]["following"].Add(target);
+
+                        }
+
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var sorted = vLogger.OrderByDescending(a => a.Value["followers"].Count).ThenBy(a => a.Value["following"].Count).ToDictionary(x => x.Key, x => x.Value);
+            int i = 1;
+
+            Console.WriteLine($"The V-Logger has a total of {vLogger.Count} vloggers in its logs.");
+
+            foreach (var entry in sorted)
+            {
+                Console.WriteLine($"{i}. {entry.Key} : {entry.Value["followers"].Count} followers, {entry.Value["following"].Count} following");
+                if (i == 1)
+                {
+                    foreach (var follower in entry.Value["followers"])
+                    {
+                        Console.WriteLine($"*  {follower}");
+                    }
+                }
+
+                i++;
+            }
+        }
+    }
+}
---
+++ C# Advanced/03. Sets and Dictionaries/Workshop/08.Ranking/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Ranking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> contests = new Dictionary<string, string>();
+            SortedDictionary<string, Dictionary<string, int>> scores = new SortedDictionary<string, Dictionary<string, int>>();
+            string input = Console.ReadLine();
+
+            while (input != "end of contests")
+            {
+                string[] tokens = input.Split(':');
+                contests[tokens[0]] = tokens[1];
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "end of submissions")
+            {
+                string[] tokens = input.Split("=>");
+                string contest = tokens[0];
+                string password = tokens[1];
+                string username = tokens[2];
+                int points = int.Parse(tokens[3]);
+
+                if (contests.ContainsKey(contest) && contests[contest] == password)
+                {
+                    if (!scores.ContainsKey(username))
+                    {
+                        scores[username] = new Dictionary<string, int>();
+                    }
+
+                    if (!scores[username].ContainsKey(contest))
+                    {
+                        scores[username][contest] = 0;
+                    }
+
+                    if (scores[username][contest] < points)
+                    {
+                        scores[username][contest] = points;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var bestCandidate = scores.OrderByDescending(a => a.Value.Values.Sum()).Take(1).ToDictionary(a => a.Key, a => a.Value).First();
+
+            Console.WriteLine($"Best candidate is {bestCandidate.Key} with total {bestCandidate.Value.Values.Sum()} points.");
+
+            Console.WriteLine("Ranking:");
+
+            foreach (var entry in scores)
+            {
+                Console.WriteLine(entry.Key);
+                foreach (var subentry in entry.Value.OrderByDescending(a => a.Value))
+                {
+                    Console.WriteLine($"#  {subentry.Key} -> {subentry.Value}");
+                }
+            }
+        }
+    }
+}
