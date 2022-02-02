4b9ac1af4ed73cf3aac2e2a3dc9decf56208034c
KChavdarov <k.chavdarov92@gmail.com>
2022-02-02 02:09:15 +0200

    completed bonus task

---
+++ C# Advanced/06. Defining Classes/Workshop/SoftUniParking/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    internal class Car
+    {
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNumber;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public string RegistrationNumber { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Make: {Make}");
+            result.AppendLine($"Model: {Model}");
+            result.AppendLine($"HorsePower: {HorsePower}");
+            result.AppendLine($"RegistrationNumber: {RegistrationNumber}");
+            return result.ToString().Trim();
+        }
+    }
+}
---
+++ C# Advanced/06. Defining Classes/Workshop/SoftUniParking/Parking.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniParking
+{
+    internal class Parking
+    {
+        private Dictionary<string, Car> cars;
+
+        private int capacity;
+
+        public Dictionary<string, Car> Cars
+        {
+            get { return cars; }
+            set { cars = value; }
+        }
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            cars = new Dictionary<string, Car>(this.capacity);
+        }
+
+        public int Count { get => Cars.Count; }
+
+        public string AddCar(Car car)
+        {
+            if (Cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            else
+            {
+                if (Cars.Count == capacity)
+                {
+                    return "Parking is full!";
+                }
+                else
+                {
+                    Cars.Add(car.RegistrationNumber, car);
+                    return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+                }
+            }
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (Cars.ContainsKey(registrationNumber))
+            {
+                cars.Remove(registrationNumber);
+                return $"Successfully removed {registrationNumber}";
+            }
+            else
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return Cars.FirstOrDefault(a => a.Value.RegistrationNumber == registrationNumber).Value;
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (string registrationNumber in new HashSet<string>(registrationNumbers))
+            {
+                RemoveCar(registrationNumber);
+            }
+        }
+    }
+}
