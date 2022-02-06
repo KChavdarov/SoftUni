875192a2a973459884d264a8b8b799a32234ad17
KChavdarov <k.chavdarov92@gmail.com>
2022-02-07 00:19:41 +0200

    implemented custom stack

---
+++ C# Advanced/07. Custom Data Structures/Custom Stack/Custom Stack/CustomStack.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Custom_Stack
+{
+    internal class CustomStack<T>
+    {
+        private const int initialSize = 4;
+        private T[] stack = new T[initialSize];
+
+        public int Count { get; private set; } = 0;
+
+        public void Push(T value)
+        {
+            if (stack.Length == Count)
+            {
+                Resize();
+            }
+
+            stack[Count] = value;
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack empty");
+            }
+
+            T result = stack[Count - 1];
+            stack[Count - 1] = default(T);
+            Count--;
+            if (Count < stack.Length / 4)
+            {
+                Shrink();
+            }
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack empty");
+            }
+
+            return stack[Count - 1];
+        }
+
+        private void Resize()
+        {
+            var resized = new T[stack.Length * 2];
+            for (int i = 0; i < stack.Length; i++)
+            {
+                resized[i] = stack[i];
+            }
+
+            stack = resized;
+        }
+
+        private void Shrink()
+        {
+            var shrunken = new T[Count];
+
+            for (int i = 0; i < Count; i++)
+            {
+                shrunken[i] = stack[i];
+            }
+
+            stack = shrunken;
+        }
+    }
+}
---
+++ C# Advanced/07. Custom Data Structures/Custom Stack/Custom Stack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Custom_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new CustomStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
