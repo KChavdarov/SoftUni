78030debda3a7b2d8140bea68f3fa4b3d0f1e87a
KChavdarov <k.chavdarov92@gmail.com>
2022-02-07 02:04:34 +0200

    implemented custom queue

---
+++ C# Advanced/07. Custom Data Structures/Custom Queue/Custom Queue/CustomQueue.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Custom_Queue
+{
+    internal class CustomQueue<T>
+    {
+        private const int initialSize = 4;
+        private const int initialHead = 0;
+        private const int initialTail = -1;
+        private T[] queue;
+        private int head;
+        private int tail;
+        public int Count { get => tail - head + 1; }
+
+        public CustomQueue()
+        {
+            queue = new T[initialSize];
+            head = initialHead;
+            tail = initialTail;
+        }
+
+        public void Enqueue(T value)
+        {
+            if (queue.Length - 1 == tail)
+            {
+                Resize();
+            }
+
+            queue[tail + 1] = value;
+            tail++;
+        }
+
+        public T Dequeue()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Queue empty");
+            }
+
+            T result = queue[head];
+            queue[head] = default(T);
+            head++;
+
+
+            if (queue.Length >= Count * 3)
+            {
+                Shrink();
+            }
+            else if (head > Count)
+            {
+                ShiftLeft();
+            }
+
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack empty");
+            }
+
+            return queue[head];
+        }
+
+        private void ShiftLeft()
+        {
+            int count = Count;
+            for (int i = head; i <= tail; i++)
+            {
+                queue[i - head] = queue[i];
+                queue[i] = default(T);
+            }
+
+            head = 0;
+            tail = count - 1;
+        }
+
+        private void Resize()
+        {
+            var resized = new T[queue.Length * 2];
+            for (int i = head; i <= tail; i++)
+            {
+                resized[i - head] = queue[i];
+            }
+
+            queue = resized;
+        }
+
+        public void Shrink()
+        {
+            int count = Count;
+            var shrunken = new T[Count];
+            for (int i = head; i <= tail; i++)
+            {
+                shrunken[i - head] = queue[i];
+            }
+
+            queue = shrunken;
+            head = 0;
+            tail = count - 1;
+        }
+
+        public void Clear()
+        {
+            queue = new T[initialSize];
+            head = initialHead;
+            tail = initialTail;
+        }
+    }
+}
---
+++ C# Advanced/07. Custom Data Structures/Custom Queue/Custom Queue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Custom_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new CustomQueue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+            queue.Enqueue(7);
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Enqueue(8);
+            queue.Enqueue(9);
+            queue.Dequeue();
+            queue.Dequeue();
+        }
+    }
+}
