016d9fa3d8b7095d15f78bb9050e408fd52040b1
KChavdarov <k.chavdarov92@gmail.com>
2022-02-28 01:39:41 +0200

    completed lab

---
+++ C# OOP/01. Inheritance/Lab/RandomList/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomList
+{
+    internal class RandomList<T> : List<T>
+    {
+        private Random random = new Random();
+
+        public T GetRandomItem()
+        {
+            return this[random.Next(0, Count)];
+        }
+
+        public void DeleteRandomItem()
+        {
+            RemoveAt(random.Next(0, Count));
+        }
+    }
+}
---
+++ C# OOP/01. Inheritance/Lab/StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackOfStrings
+{
+    internal class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> elements)
+        {
+            foreach (var element in elements)
+            {
+                Push(element);
+            }
+        }
+    }
+}
