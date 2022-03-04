a4438fb39a92813ce06dd4c93faaea36f14cf6fb
KChavdarov <k.chavdarov92@gmail.com>
2022-03-04 02:00:20 +0200

    completed lab

---
+++ C# OOP/02. Encapsulation/Lab/01.Persons/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Persons
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Team team = new Team("SoftUni");
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        decimal.Parse(cmdArgs[3]));
+
+                persons.Add(person);
+            }
+            //var parcentage = decimal.Parse(Console.ReadLine());
+            //persons.ForEach(p => p.IncreaseSalary(parcentage));
+            //persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+            foreach (var person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine(team.FirstTeam.Count);
+            Console.WriteLine(team.ReserveTeam.Count);
+        }
+
+    }
+}
---
+++ C# OOP/02. Encapsulation/Lab/01.Persons/Team.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Persons
+{
+    internal class Team
+    {
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public string Name { get; set; }
+        public IReadOnlyCollection<Person> FirstTeam => firstTeam;
+        public IReadOnlyCollection<Person> ReserveTeam => reserveTeam;
+
+        public Team(string name)
+        {
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+            Name = name;
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
