46cf096b466387897227abdda911467c19276b94
KChavdarov <k.chavdarov92@gmail.com>
2023-04-07 00:48:05 +0300

    completed stacks and queues revision

--- C# Advanced/00. Revision/01. Stacks and Queues/StacksAndQueues/StacksAndQueues/Program.cs
+++ C# Advanced/00. Revision/01. Stacks and Queues/StacksAndQueues/StacksAndQueues/Program.cs
@@ -1,4 +1,6 @@
-﻿TrafficJam();
+﻿using System.Linq;
+
+CupsAndBottles();
 
 void ReverseStrings()
 {
@@ -228,4 +230,291 @@ void TrafficJam()
     }
 
     Console.WriteLine($"{counter} cars passed the crossroads.");
+}
+
+void BasicStackOperations()
+{
+    var nsx = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+    int n = nsx[0];
+    int s = nsx[1];
+    int x = nsx[2];
+    var input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+    Stack<int> numbers = new Stack<int>();
+
+    for (int i = 0; i < n; i++)
+    {
+        numbers.Push(input[i]);
+    }
+
+    for (int i = 0; i < s; i++)
+    {
+        if (numbers.Count > 0)
+        {
+            numbers.Pop();
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (numbers.Contains(x))
+    {
+        Console.WriteLine("true");
+        return;
+    }
+
+    if (numbers.Count == 0)
+    {
+        Console.WriteLine(0);
+        return;
+    }
+
+    int min = int.MaxValue;
+    while (numbers.Count > 0)
+    {
+        int num = numbers.Pop();
+        min = Math.Min(min, num);
+    }
+    Console.WriteLine(min);
+}
+
+void BasicQueueOperations()
+{
+    var nsx = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+    int n = nsx[0];
+    int s = nsx[1];
+    int x = nsx[2];
+    var input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+    Queue<int> numbers = new Queue<int>();
+
+    for (int i = 0; i < n; i++)
+    {
+        numbers.Enqueue(input[i]);
+    }
+
+    for (int i = 0; i < s; i++)
+    {
+        if (numbers.Count > 0)
+        {
+            numbers.Dequeue();
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (numbers.Contains(x))
+    {
+        Console.WriteLine("true");
+        return;
+    }
+
+    if (numbers.Count == 0)
+    {
+        Console.WriteLine(0);
+        return;
+    }
+
+    int min = int.MaxValue;
+    while (numbers.Count > 0)
+    {
+        int num = numbers.Dequeue();
+        min = Math.Min(min, num);
+    }
+
+    Console.WriteLine(min);
+}
+
+void FastFood()
+{
+    int food = int.Parse(Console.ReadLine());
+    var orders = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+    Console.WriteLine(orders.Max());
+
+    while (orders.Count > 0)
+    {
+        if (food >= orders.Peek())
+        {
+            food -= orders.Dequeue();
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (orders.Count == 0)
+    {
+        Console.WriteLine("Orders complete");
+    }
+    else
+    {
+        Console.Write("Orders left:");
+        while (orders.Count > 0)
+        {
+            Console.Write($" {orders.Dequeue()}");
+        }
+    }
+}
+
+void Crossroads()
+{
+    var queue = new Queue<string>();
+    int greenLightDuration = int.Parse(Console.ReadLine());
+    int freeWindowDuration = int.Parse(Console.ReadLine());
+    var crossroad = new Queue<char>();
+    string input = Console.ReadLine();
+    int carsPassed = 0;
+
+    while (input != "END")
+    {
+        if (input != "green")
+        {
+            queue.Enqueue(input);
+        }
+        else
+        {
+            string car = string.Empty;
+            for (int i = 0; i < greenLightDuration; i++)
+            {
+                if (crossroad.Count > 0)
+                {
+                    crossroad.Dequeue();
+                }
+                else
+                {
+                    if (queue.Count > 0)
+                    {
+                        carsPassed++;
+                        car = queue.Dequeue();
+                        crossroad = new Queue<char>(car);
+                        crossroad.Dequeue();
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < freeWindowDuration; i++)
+            {
+                if (crossroad.Count > 0)
+                {
+                    crossroad.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (crossroad.Count > 0)
+            {
+                Console.WriteLine("A crash happened!");
+                Console.WriteLine($"{car} was hit at {crossroad.Dequeue()}.");
+                return;
+            }
+        }
+
+        input = Console.ReadLine();
+    }
+
+    Console.WriteLine("Everyone is safe.");
+    Console.WriteLine($"{carsPassed} total cars passed the crossroads.");
+}
+
+void KeyRevolver()
+{
+    int bulletPrice = int.Parse(Console.ReadLine());
+    int barrelSize = int.Parse(Console.ReadLine());
+    var bullets = new Stack<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+    var locks = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+    int intel = int.Parse(Console.ReadLine());
+    int bulletCount = 0;
+    int ammoCount = barrelSize;
+
+    while (locks.Count > 0)
+    {
+        if (bullets.Count > 0)
+        {
+            int bulletValue = bullets.Pop();
+            bulletCount++;
+            ammoCount--;
+
+            int lockValue = locks.Peek();
+
+            if (lockValue >= bulletValue)
+            {
+                Console.WriteLine("Bang!");
+                locks.Dequeue();
+            }
+            else
+            {
+                Console.WriteLine("Ping!");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            return;
+        }
+
+        if (ammoCount == 0 && bullets.Count > 0)
+        {
+            ammoCount = barrelSize;
+            Console.WriteLine("Reloading!");
+        }
+    }
+
+    int moneyEarned = intel - bulletCount * bulletPrice;
+    Console.WriteLine($"{bullets.Count} bullets left. Earned ${moneyEarned}");
+
+}
+
+void CupsAndBottles()
+{
+    Queue<int> cups = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+    Stack<int> bottles = new Stack<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+    int wastedWater = 0;
+
+    while (bottles.Any() && cups.Any())
+    {
+        int cup = cups.Peek();
+
+        while (cup > 0)
+        {
+            if (bottles.Any())
+            {
+                int bottle = bottles.Pop();
+                if (bottle >= cup)
+                {
+                    wastedWater += bottle - cup;
+                    cup = 0;
+                    cups.Dequeue();
+                }else
+                {
+                    cup -= bottle;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    if (cups.Count > 0 && bottles.Count == 0)
+    {
+        Console.WriteLine($"Cups: {String.Join(' ', cups)}");
+    }
+    else
+    {
+        Console.WriteLine($"Bottles: {String.Join(' ', bottles)}");
+    }
+
+    Console.WriteLine($"Wasted litters of water: {wastedWater}");
 }
