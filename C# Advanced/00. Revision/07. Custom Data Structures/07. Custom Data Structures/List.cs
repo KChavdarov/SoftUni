29ef03f1f37ed56863e0fbd5c69d894c5706b260
KChavdarov <k.chavdarov92@gmail.com>
2023-05-07 01:21:08 +0300

    implemented custom list

---
+++ C# Advanced/00. Revision/07. Custom Data Structures/07. Custom Data Structures/List.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._Custom_Data_Structures
+{
+    public class List<T>
+    {
+        private const byte initialCapacity = 2;
+        private T[] array = new T[initialCapacity];
+
+        public int Count { get; private set; } = 0;
+        public T this[int index]
+        {
+            get
+            {
+                if (!isValidIndex(index))
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                else
+                {
+                    return array[index];
+                }
+            }
+            set
+            {
+                if (!isValidIndex(index))
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                else
+                {
+                    array[index] = value;
+                }
+            }
+        }
+        public void Add(T item)
+        {
+            array[Count++] = item;
+
+            if (array.Length == Count)
+            {
+                Resize();
+            }
+        }
+        public T RemoveAt(int index)
+        {
+            if (!isValidIndex(index))
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                T result = array[index];
+                ShiftLeft(index);
+                Count--;
+                if (array.Length > Count * 4) { Shrink(); };
+                return result;
+            }
+        }
+        public bool Contains(T item)
+        {
+            return array.Take(Count).Contains(item);
+        }
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            if (!isValidIndex(firstIndex) || !isValidIndex(secondIndex))
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                T temp = array[firstIndex];
+                array[firstIndex] = array[secondIndex];
+                array[secondIndex] = temp;
+            }
+        }
+        public void Insert(int index, T item)
+        {
+            if (!isValidIndex(index))
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                ShiftRight(index);
+                array[index] = item;
+                if (array.Length == ++Count)
+                {
+                    Resize();
+                }
+            }
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count; i++)
+            {
+                array[i] = array[i + 1];
+            }
+        }
+        private void ShiftRight(int index)
+        {
+            for (int i = Count; i > index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+        }
+        private void Shrink()
+        {
+            var tempArray = new T[array.Length / 2];
+            for (int i = 0; i < Count; i++)
+            {
+                tempArray[i] = array[i];
+            }
+            array = tempArray;
+        }
+        private void Resize()
+        {
+            var tempArray = new T[array.Length * 2];
+            for (int i = 0; i < Count; i++)
+            {
+                tempArray[i] = array[i];
+            }
+            array = tempArray;
+        }
+        private bool isValidIndex(int index)
+        {
+            return index >= 0 && index < Count;
+        }
+    }
+}
--- C# Advanced/00. Revision/07. Custom Data Structures/07. Custom Data Structures/Program.cs
+++ C# Advanced/00. Revision/07. Custom Data Structures/07. Custom Data Structures/Program.cs
@@ -1,13 +1,24 @@
 ﻿using _07._Custom_Data_Structures;
 
-var LinkedList = new DoublyLinkedList<int>();
+//var LinkedList = new DoublyLinkedList<int>();
+//LinkedList.AddFirst(2);
+//LinkedList.AddFirst(1);
+//LinkedList.AddFirst(0);
+//LinkedList.AddLast(3);
+//LinkedList.AddLast(4);
+//LinkedList.RemoveFirst();
+//LinkedList.RemoveLast();
+//LinkedList.ForEach(Console.WriteLine);
+//Console.WriteLine(string.Join(" ", LinkedList.ToArray()));
 
-LinkedList.AddFirst(2);
-LinkedList.AddFirst(1);
-LinkedList.AddFirst(0);
-LinkedList.AddLast(3);
-LinkedList.AddLast(4);
-LinkedList.RemoveFirst();
-LinkedList.RemoveLast();
-LinkedList.ForEach(Console.WriteLine);
-Console.WriteLine(string.Join(" ", LinkedList.ToArray()));
+var list = new _07._Custom_Data_Structures.List<int>();
+list.Add(0);
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(4);
+list.Add(5);
+list.RemoveAt(0);
+list.Swap(0, 3);
+list.Insert(4, 6);
+Console.WriteLine(list.Contains(2));
