1a6b527943b5f6c94ffd46d943268e84cd5a342c
Kiril Chavdarov <k.chavdarov92@gmail.com>
2025-02-17 23:58:27 +0200

    practiced fetch API

--- JS Applications/02. Asynchronous Programming/01.Bus-Stop/app.js
+++ JS Applications/02. Asynchronous Programming/01.Bus-Stop/app.js
@@ -3,28 +3,59 @@ const busesUl = document.getElementById('buses');
 const stopName = document.getElementById('stopName');
 
 function getInfo() {
-    fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId.value}`)
-        .then((response) => {
+    let url = `http://localhost:3030/jsonstore/bus/businfo/${stopId.value}`;
+    fetch(url)
+        .then(response => {
             if (!response.ok) {
                 throw new Error(response.statusText);
             }
-            return response.json();
+            return response.json()
         })
-        .then(showStopInfo)
-        .catch((error) => {
-            console.log(error);
+        .then(displayInfo)
+        .catch(error => {
+            console.error(error);
             busesUl.textContent = '';
             stopName.textContent = 'Error';
-        });
+        })
 }
 
-function showStopInfo(data) {
+function displayInfo({ name, buses }) {
     busesUl.textContent = '';
-    stopName.textContent = data.name;
-    Object.entries(data.buses).forEach(([busId, time]) => {
-        const newLi = document.createElement('li');
-        newLi.textContent = `Bus ${busId} arrives in ${time} minutes`;
-        busesUl.appendChild(newLi);
-        stopId.value = '';
-    });
+    stopName.textContent = name;
+    Object.entries(buses).forEach(createBusLi);
 }
+
+function createBusLi([busId, time]) {
+    let item = document.createElement('li');
+    item.textContent = `Bus ${busId} arrives in ${time}`
+    busesUl.appendChild(item)
+    return item;
+}
+
+
+// function getInfo() {
+//     fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId.value}`)
+//         .then((response) => {
+//             if (!response.ok) {
+//                 throw new Error(response.statusText);
+//             }
+//             return response.json();
+//         })
+//         .then(showStopInfo)
+//         .catch((error) => {
+//             console.log(error);
+//             busesUl.textContent = '';
+//             stopName.textContent = 'Error';
+//         });
+// }
+
+// function showStopInfo(data) {
+//     busesUl.textContent = '';
+//     stopName.textContent = data.name;
+//     Object.entries(data.buses).forEach(([busId, time]) => {
+//         const newLi = document.createElement('li');
+//         newLi.textContent = `Bus ${busId} arrives in ${time} minutes`;
+//         busesUl.appendChild(newLi);
+//         stopId.value = '';
+//     });
+// }
